df0ece10695ef7722bef6a5ded0daab2e5ce8fa4
xiangwen <xiangwenwe@foxmail.com>
2013-11-15 18:27:08 +0800

    更新browser模块
    更新browser模块

---
+++ src/browser.js
@@ -0,0 +1,23 @@
+/*
+* mobile ui browser 依赖 zepto.js  作者：wenren
+*/
+(function($){
+  $.browser = {
+      IE: !!(window.attachEvent && navigator.userAgent.indexOf('Opera') === -1),
+      Opera: navigator.userAgent.indexOf('Opera') > -1,
+      //检测浏览器是否为WebKit内核
+      WebKit: navigator.userAgent.indexOf('AppleWebKit/') > -1,
+      //检测浏览器是否为Gecko内核，如Firefox
+      Gecko: navigator.userAgent.indexOf('Gecko') > -1 && navigator.userAgent.indexOf('KHTML') === -1,
+      MobileSafari: !!navigator.userAgent.match(/Apple.*Mobile.*Safari/),
+      android: navigator.userAgent.indexOf('Android') > -1 || navigator.userAgent.indexOf('Linux') > -1
+  }
+  $.webPageReg = {
+      //获取网页body部份代码
+      body:/<body[^>]*>([\s\S]*)<\/body>/i,
+      //获取网页style部份代码
+      style:/<style[^>]*>([\s\S]*)<\/style>/i,
+      //获取网页script部份代码
+      script:/<script\b[^<]*(?:(?!<\/script)<[^>]*)*<\/script>/gi
+  }
+})(Zepto);
