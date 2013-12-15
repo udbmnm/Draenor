98cc0857fa478cfcad0886edaeaabe6424aeca00
xiangwenwe <xiangwenwe@foxmail.com>
2013-12-15 15:39:00 +0800

    删除部分代码
    删除部分代码

---
+++ src/webPageReg.js
@@ -0,0 +1,13 @@
+/*
+* mobile ui 网页抓取正则 依赖 zepto.js  作者：wenren
+*/
+(function($){
+  $.webPageReg = {
+      //获取网页body部份代码
+      body:/<body[^>]*>([\s\S]*)<\/body>/i,
+      //获取网页style部份代码
+      style:/<style[^>]*>([\s\S]*)<\/style>/i,
+      //获取网页script部份代码
+      script:/<script\b[^<]*(?:(?!<\/script)<[^>]*)*<\/script>/gi
+  }
+})(Zepto);
